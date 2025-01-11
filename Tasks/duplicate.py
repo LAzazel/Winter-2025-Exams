40fe606ab0304561cc80d41b174577981d27ec78
LAzazel <90055165+LAzazel@users.noreply.github.com>
2025-01-11 03:52:01 +0200

    Update and rename duplicate.js to duplicate.py
    Translated from JS to Python.

---
+++ Tasks/duplicate.py
@@ -0,0 +1,11 @@
+// Refactor following solution
+// Return an array without duplicates
+
+def duplicate(value, N):
+    if N <= 0:
+        return []
+    else:
+        res = []
+        for i in range(N):
+            res.append(value)
+        return res

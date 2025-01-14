cbc4368cd30b55d42c7b3532de82e57e1130bae6
LAzazel <90055165+LAzazel@users.noreply.github.com>
2025-01-14 06:34:13 +0200

    Update plane.py

--- Tasks/plane.py
+++ Tasks/plane.py
@@ -3,17 +3,15 @@ Refactor following solution
 Make nested array plane
 """
 
-def const_plane(arr, res=None):
-    if res is None:
-        res = []
-    j = 0
+def const_plane(arr):
+    res = []
+
     for i in range(len(arr)):
         value = arr[i]
-        j = i
         if isinstance(value, list):
             res.extend(const_plane(value))
             arr[i] = res[i - 1] if i > 0 else None
         else:
-            arr[i] = res[j - 1] if j > 0 else None
+            arr[i] = res[i - 1] if i > 0 else None
             res.append(value)
     return res

f145abf2313429cfe05e1dcb197394c912a41a71
LAzazel <90055165+LAzazel@users.noreply.github.com>
2025-01-14 07:07:24 +0200

    Update range.py

--- Tasks/range.py
+++ Tasks/range.py
@@ -5,6 +5,5 @@ Generate int array from given range
 
 def _range(from_value, to_value):
     if to_value >= from_value:
-        res = [i for i in range(from_value, to_value+1)]
-        return res
+        return [i for i in range(from_value, to_value+1)]
     return []

c4b93b4e4ab70344a4795c4c434daeff4cc5e4e7
LAzazel <90055165+LAzazel@users.noreply.github.com>
2025-01-14 06:03:02 +0200

    Update plane.js
    Translated to Python.

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,19 +1,19 @@
-// Refactor following solution
-// Make nested array plane
+"""
+Refactor following solution
+Make nested array plane
+"""
 
-const_plane = function(arr, res=[]) {
-  j = 0; for(let i = 0, length = arr.length; i < length; i++){
-    value = arr[i];
-    j = i
-    if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
-    res.push(value);
-    };
-  };
-  return res
-}
-
-module.exports = const_plane;
+def const_plane(arr, res=None):
+    if res is None:
+        res = []
+    j = 0
+    for i in range(len(arr)):
+        value = arr[i]
+        j = i
+        if isinstance(value, list):
+            res.extend(const_plane(value))
+            arr[i] = res[i - 1] if i > 0 else None
+        else:
+            arr[i] = res[j - 1] if j > 0 else None
+            res.append(value)
+    return res

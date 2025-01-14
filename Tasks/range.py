37aa859b814886e48fc427416d898d22fe9696f4
LAzazel <90055165+LAzazel@users.noreply.github.com>
2025-01-14 06:45:53 +0200

    Update and rename range.js to range.py
    Translated to Python.

---
+++ Tasks/range.py
@@ -0,0 +1,13 @@
+"""
+Refactor following solution
+Generate int array from given range
+"""
+
+def _range(from_value, to_value):
+    if to_value >= from_value:
+        res = [None] * (to_value - from_value + 1)
+        for i in range(from_value, to_value + 1):
+            res[i - from_value] = i
+        return res
+    else:
+        return []
